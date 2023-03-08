bde7999bfbc2830e6beb33bb553b196a4c6ac76e
Dani Visa <105554997+DaniV22@users.noreply.github.com>
2023-03-09 00:21:31 +0100

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -80,8 +80,7 @@ class OptimizerAdam:
         update_parameters :     updates weights and biases of the given layer
         iteration :             increases the iterations by one unit
     '''
-
-    # Initialize optimizer - set settings
+    
     def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, beta_1=0.9, beta_2=0.999):
         self.learning_rate = learning_rate
         self.decay = decay
@@ -89,8 +88,7 @@ class OptimizerAdam:
         self.epsilon = epsilon
         self.beta_1 = beta_1
         self.beta_2 = beta_2
-
-    # Call once before any parameter updates
+        
     def update_learning_rate(self):
         self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay*self.iterations))
 
@@ -123,6 +121,5 @@ class OptimizerAdam:
         layer.weights += -self.learning_rate*weight_momentums_corrected / (np.sqrt(weight_second_moments_corrected) + self.epsilon)
         layer.biases += -self.learning_rate*bias_momentums_corrected / (np.sqrt(bias_second_moments_corrected) + self.epsilon)
 
-    # Call once after any parameter updates
     def iteration(self):
         self.iterations += 1
